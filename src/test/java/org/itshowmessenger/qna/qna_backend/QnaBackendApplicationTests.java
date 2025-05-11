cad3594346f776dc3f9b2cb4afca48d61166800e
fldus <kry59965@gmail.com>
2025-05-12 00:58:21 +0900

    기본 설정

---
+++ src/test/java/org/itshowmessenger/qna/qna_backend/QnaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itshowmessenger.qna.qna_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
