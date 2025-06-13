67f3314bfe4fec2da2a8fc0d51723bf276b8deb5
fldus <kry59965@gmail.com>
2025-06-14 02:09:40 +0900

    웹소켓 설정

---
+++ src/main/java/org/itshow/messenger/qna_backend/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package org.itshow.messenger.qna_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat")    // WebSocket 연결 주소
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue");  // 메시지 수신 구독
+        registry.setApplicationDestinationPrefixes("/app"); // 메시지 전송
+    }
+}
